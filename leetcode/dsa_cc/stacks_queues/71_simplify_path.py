a26dd4b705a4dc3502bf2972d89b87f072e234de
Ryan Weidinger <r.weidinger11@gmail.com>
2024-09-19 00:33:55 -0500

    Placeholder initial

---
+++ leetcode/dsa_cc/stacks_queues/71_simplify_path.py
@@ -0,0 +1,11 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+
+        # Initialize stack
+        stack = []
+        
+        # Loop through stack and look for the following:
+        # - ".."
+        for char in path:
+
+            pass
