814c30ccd268350b9d0f2efd65aefc24eca300af
Ryan Weidinger <r.weidinger11@gmail.com>
2024-10-05 09:49:40 -0500

    Initial commit

---
+++ leetcode/dsa_cc/trees_graphs/graphs/863_nd_dist_k_bin_tr.py
@@ -0,0 +1,13 @@
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        
+        
