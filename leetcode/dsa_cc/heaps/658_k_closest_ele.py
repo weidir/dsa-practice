4d02945c9244865d1546ec88512aa255c3f6d5db
Ryan Weidinger <r.weidinger11@gmail.com>
2024-10-12 20:10:15 -0500

    Initial commit

---
+++ leetcode/dsa_cc/heaps/658_k_closest_ele.py
@@ -0,0 +1,21 @@
+from typing import List
+import heapq
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+
+        def bin_search(arr, x):
+            pass
+        
+
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    arr1 = [1,2,3,4,5]
+    k1 = 4, 
+    x1 = 3
+    ans1 = sol.findClosestElements(arr1, k1, x1)
+    print(ans1)
