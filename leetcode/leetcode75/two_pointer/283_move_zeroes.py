bde34f84cc97428482d790083edc89b60fd9f830
Ryan Weidinger <r.weidinger11@gmail.com>
2024-11-13 12:28:23 -0600

    Added another solution

--- leetcode/leetcode75/two_pointer/283_move_zeroes.py
+++ leetcode/leetcode75/two_pointer/283_move_zeroes.py
@@ -1,24 +1,55 @@
 from typing import List
 
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+    def moveZeroesNaive(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
 
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                nums.remove(0)
-                nums.append(0)
+        ans = []
+        zeros_seen = 0
+
+        for num in nums:
+            if num != 0:
+                ans.append(num)
+            else:
+                zeros_seen += 1
+        
+        ans.extend([0] * zeros_seen)
+        nums[:] = ans
+
+    def moveZeroesOptimal(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        nums_len = len(nums)
+
+        l = 0
+        r = 1
+
+        while r < nums_len:
+            if nums[r] != 0:
+                while nums[l] != 0 and l < r:
+                    l += 1
+                l_val = nums[l]
+                nums[l] = nums[r]
+                nums[r] = l_val
+
+                r += 1
+                l += 1
+            else:
+                r += 1
                 
 
 if __name__ == "__main__":
     sol = Solution()
 
     nums1 = [0,1,0,3,12]
-    sol.moveZeroes(nums1)
+    # sol.moveZeroesOptimal(nums1)
+    sol.moveZeroesNaive(nums1)
     print(nums1)
 
     nums2 = [0,0,1]
-    sol.moveZeroes(nums2)
+    sol.moveZeroesOptimal(nums2)
     print(nums2)
